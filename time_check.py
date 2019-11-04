8d1ba76b8598633c5077a369d11361ac57055d20
danthony25 <54610907+danthony25@users.noreply.github.com>
2019-11-03 22:38:37 -0700

    Delete time_check.py

--- time_check.py
+++
@@ -1,7 +0,0 @@
-import time
-max_time = int(input('Enter the amount of seconds you want to run this: '))
-start_time = time.time()  # remember when we started
-while (time.time() - start_time) < max_time:
-    for i in range(1,10):
-        print(i)
-        
